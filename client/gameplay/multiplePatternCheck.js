f9a37f55ca8f6a6813d9423a8c42d5b92f599230
Daniel Co <dcohomemail@gmail.com>
2020-10-03 17:57:24 -0700

    create function to check if multiple cards are valid pairs,triples, quads, or straight

---
+++ client/gameplay/multiplePatternCheck.js
@@ -0,0 +1,35 @@
+import checkCardValue from './checkCardValue';
+/* 
+    Used to check when pairs, triples, quads are played, they are
+    actually matching numbers/faces or they are sequential for a straight 
+    @param {string[]} cards - a list of card names
+*/
+const multiplePatternCheck = (cards) => {
+    const sequence = '345678910JQKA2';
+
+    const values = cards.map((card) => {
+        if (card.length === 2) {
+            return card.slice(0,1);
+        } else {
+            return card.slice(0,2);
+        }
+    });
+
+    if (values.length < 3) { // 2 cards, check if it's a pair
+        return values.every(val => val === values[0]);
+    } else if (values.length === 3) { // 3 cards, check if it's a triple or straight
+        if (values.every(val => val === values[0])) {
+            return true;
+        } else {
+            return sequence.indexOf(values.join('')) !== -1 ? true : false
+        }
+    } else { // more than 4 cards, check if it's quad or straight
+        if (values.every(val => val === values[0])) {
+            return true;
+        } else {
+            return sequence.indexOf(values.join('')) !== -1 ? true : false;
+        }
+    }
+}
+
+export default multiplePatternCheck;
